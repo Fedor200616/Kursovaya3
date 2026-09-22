f5961f06eca0e666925d0664ae7b513a43cc2c49
Fedor <50566270+Fedor200616@users.noreply.github.com>
2026-09-22 10:49:33 +0300

    Добавлены функции открытия файлов

--- FileWork.h
+++ FileWork.h
@@ -1,1 +1,12 @@
+#include <filesystem>
+#include <iostream>
 
+namespace fs = std::filesystem;
+
+namespace FileStorage {
+    bool loadFromFile (const fs::path& path, std::vector<StudentInfo>& Info); 
+    bool saveToFile (const fs::path& path, std::vector<StudentInfo>& Info);
+
+    bool createBackup(const fs::path& main_path, const std::vector<StudentInfo>& Info);
+    bool removeBackup(const std::string& main_path);
+}
