4a5da9d9fcec049c0e9b0d470c108216917f43cd
Fedor <50566270+Fedor200616@users.noreply.github.com>
2026-09-22 10:42:23 +0300

    Добавлены структуры и массивы состояний

--- Status.h
+++ Status.h
@@ -1,1 +1,22 @@
+#include <string>
+#include <filesystem>
+#include <vector>
 
+//Запись о студенте
+struct StudentInfo {
+    std::string name;
+    std::string group;
+    std::string pass;
+    std::string num;
+}
+
+struct WorkState {
+    std::vector<StudentInfo> info; //Массив данных об учениках
+    filesystem::path file_filepath; // Путь к файлу информации об учениках
+    filesystem::path exe_filepath; //Путь к exe файлу программы
+    bool is_modified; // Был ли модифицирован файл
+
+    size_t sel_row = 0; // Выбранная строка
+    StudentInfo sel_inf = 0; //выбранная информация
+    size_t page_top = 0; // Первая видимая строка в отображении
+}
